b6e104a94969e3f5b1141de38977c10b6f0fdbf4
GittySam72 <61106190+GittySam72@users.noreply.github.com>
2022-09-18 03:57:09 +0530

    designed status of single mode

--- modes.py
+++ modes.py
@@ -3,6 +3,7 @@
 from email.policy import default
 from operator import index
 from tkinter import CENTER, N
+from turtle import bgcolor
 import flet
 import pytube
 import threading 
@@ -17,6 +18,7 @@ from pytube import YouTube
 import os
 from flet import (
     Theme,
+    margin,
     FilePicker,
     Divider,
     VerticalDivider,
@@ -36,6 +38,7 @@ from flet import (
     colors,
     icons,
     ElevatedButton,
+    AlertDialog,
     Radio,
     RadioGroup,
     ListTile,
@@ -53,6 +56,7 @@ from flet import (
     Icon,
     NavigationRail,
     NavigationRailDestination,
+    ProgressRing
 )
 
 base_dir = '/home/greytesla/Desktop/Sam/Testing/'
@@ -171,7 +175,7 @@ def main(page: Page):
         on_change=lambda e: viewDecider(e.control.selected_index),
     )
 
-    def radiogroup_changed(e):
+    def bulk_url_state_changer(e):
         bulk_url_id.disabled = False
         page.update()
 
@@ -192,7 +196,7 @@ def main(page: Page):
 
     bulk_url_type = RadioGroup(content=Column([
         Radio(value="channel", label="Channel"),
-        Radio(value="playlist", label="Playlist")], spacing=5), on_change=radiogroup_changed)
+        Radio(value="playlist", label="Playlist")], spacing=5), on_change=bulk_url_state_changer)
 
     def get_directory_result(e: FilePickerResultEvent):
         pathText.value = e.path if e.path else "Cancelled!"
@@ -205,7 +209,10 @@ def main(page: Page):
     page.overlay.extend([get_directory_dialog])
 
     def downloadActionForSingle(e):
-        singleDownloader()
+        # single_status_thumbnail.controls.pop()
+        # single_download_status.controls.pop()
+        # page.update()
+        onSingleDownloadThread()
 
     def downloadActionForBulk(e):
         bulkViewMode.update()
@@ -215,17 +222,43 @@ def main(page: Page):
         else:
             bulk_MP4_downloader()
         
+    pr = ProgressRing(stroke_width=2)
 
-    
+    dlg = AlertDialog(
+        title=Text("Parsing Data",text_align='center'), on_dismiss=lambda e: print("Dialog dismissed!"),
+        content = pr
+    )
+    def viewDlg():
+            page.dialog = dlg
+            dlg.open = True
+            page.update()
+            sleep(3)
+            dlg.open = False
+            single_status.visible = True
+            page.update()
+
+    def onSingleDownloadThread():
+        threading.Thread(target=viewDlg).start()
+        threading.Thread(target=singleDownloader).start()
+
     def singleDownloader():
         if(downloadFormat.value == 'mp3'):
             try:
                 yt = YouTube(single_url_id.value)
-                pb = ProgressBar(width=400,color="blue",bgcolor=colors.BLACK87)
                 video = yt.streams.filter(only_audio=True).first()
-                destination = base_dir + bulk_artist_name.value
+                single_video_name.value = video.title
+                single_video_length.value = str(int(yt.length / 60 )) + " Minute"
                 fileSizeInBytes = video.filesize
                 MaxFileSize = fileSizeInBytes/1024000
+                single_video_size.value = str(int(round(MaxFileSize))) + " MB"
+                single_video_quality.value = "128Hz"
+                single_video_format.value = "MP3"
+                single_video_link.value = single_url_id.value
+                single_video_folder.value = base_dir
+                single_status.update()
+                pb = ProgressBar(width=400,color='blue',bgcolor=colors.BLACK87)
+                video = yt.streams.filter(only_audio=True).first()
+                destination = base_dir + bulk_artist_name.value
                 MB = str(int(round(MaxFileSize))) + " MB"
                 cnt = 1
                 url = yt.thumbnail_url
@@ -238,9 +271,10 @@ def main(page: Page):
                 single_status_thumbnail.controls.append(tImage)
                 single_status_thumbnail.controls.append(
                     Row(
+                        
                         controls=[
                             Column([Text( str(cnt) + '  ' + u"\u27F6" + '   Downloading  ' + yt.title, font_family=default),
-                            Row([Text(value="↳"),pb,Text("File Size = " + MB, font_family=default)])
+                            Row([Text(value="↳"),pb,Text("| File Size = " + MB, font_family=default)])
                             ])],
                             spacing=0
                             )
@@ -267,7 +301,18 @@ def main(page: Page):
         else:
             try:
                 yt = YouTube(single_url_id.value)
-                pb = ProgressBar(width=400,color="blue",bgcolor=colors.BLACK87)
+                video = yt.streams.get_highest_resolution()
+                single_video_name.value = video.title
+                single_video_length.value = str(int(yt.length / 60 )) + " Minute"
+                fileSizeInBytes = video.filesize
+                MaxFileSize = fileSizeInBytes/1024000
+                single_video_size.value = str(int(round(MaxFileSize))) + " MB"
+                single_video_quality.value = "1080P"
+                single_video_format.value = "MP4"
+                single_video_link.value = single_url_id.value
+                single_video_folder.value = base_dir
+                single_status.update()
+                pb = ProgressBar(width=400,color='blue',bgcolor=colors.BLACK87)
                 video = yt.streams.get_highest_resolution()
                 destination = base_dir + bulk_artist_name.value
                 fileSizeInBytes = video.filesize
@@ -286,7 +331,7 @@ def main(page: Page):
                     Row(
                         controls=[
                             Column([Text( str(cnt) + '  ' + u"\u27F6" + '   Downloading  ' + yt.title, font_family=default),
-                            Row([Text(value="↳"),pb,Text("File Size = " + MB, font_family=default)])
+                            Row([Text(value="↳"),pb,Text("| File Size = " + MB, font_family=default)])
                             ])],
                             spacing=0
                             )
@@ -298,7 +343,7 @@ def main(page: Page):
                 def updatePB():
                     for i in range(0, 101):
                         pb.value = i * 0.01
-                        sf = int(round(MaxFileSize)) / 20
+                        sf = int(round(MaxFileSize)) / 12
                         sleep(sf * 0.1)
                         singleModeView.update()
                 threading.Thread(target=downMp4).start()
@@ -352,7 +397,7 @@ def main(page: Page):
                             str(cnt+1) + '  ' + u"\u27F6" + '  ' + video.title + ' has been successfully downloaded.', font_family=default),
                             Text("File Size = " + MB, font_family=default)]
                     ))
-                    bulk_status_view.controls.pop()
+                    bulk_status_view.controls.remove(tImage)
                     bulkViewMode.update()
                     print(colored(video.title, 'cyan') + " has been successfully downloaded.")
                 except:
@@ -444,6 +489,7 @@ def main(page: Page):
                             Text("File Size = " + MB, font_family=default)]
                     ))
                     bulkViewMode.update()
+                    b
                     print(colored(video.title, 'cyan') + " has been successfully downloaded.")
                 except:
                     pass
@@ -505,13 +551,207 @@ def main(page: Page):
 
     bulk_status_view = Column()
     bulk_tasks_view = ListView(expand=1, spacing=10, padding=1, auto_scroll=True)
-    single_status_thumbnail = Column(width=680, height=400)
-    single_status  = Row([single_status_thumbnail],spacing=0)
+    single_status_thumbnail = Column(width=540, height=400)
+    single_download_status = Column(expand=540)
+    single_video_name = Text()
+    single_video_size = Text()
+    single_video_quality = Text()
+    single_video_format = Text()
+    single_video_link = Text(color=colors.BLUE)
+    single_video_folder = Text(no_wrap=True)
+    single_video_length = Text(no_wrap=True)
+    single_status  = Row( 
+                            visible=False,expand = True,
+                            controls= [
+                                        Column([
+                                                single_status_thumbnail,
+                                                single_download_status
+                                   ]),
+                                
+                        VerticalDivider(),
+    Card(   
+                                        expand = True,
+                                        margin=margin.all(2),
+                                        elevation = 4,
+                                        content=Column(
+        [
+        Column(
+            
+            controls=[
+                Container(
+                    content=Row([Container(
+                    content=Text(value="Video Details"),
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    
+                )], alignment="center"),
+                    
+                ),]),
+
+        Column([
+                Container(
+                    content=Row([Container(
+                    content=Text(value="Video Name"),
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                ),
+                Container(
+                    expand = True,
+                    content=single_video_name,
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                )],spacing=1, alignment="start"),
+                    bgcolor=colors.AMBER_100,
+                ),
+            ]),
+        
+        Column([
+                Container(
+                    content=Row([Container(
+                    content=Text(value="Video Length"),
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                ),
+                Container(
+                    expand = True,
+                    content=single_video_length,
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                )],spacing=1, alignment="start"),
+                    bgcolor=colors.AMBER_100,
+                ),
+            ]),
+
+        Column([
+                Container(
+                    content=Row([Container(
+                    content=Text(value="Video Size"),
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                ),
+                Container(
+                    expand = True,
+                    content=single_video_size,
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                )],spacing=1, alignment="start"),
+                    bgcolor=colors.AMBER_100,
+                ),
+            ]),
+
+        Column([
+                Container(
+                    content=Row([Container(
+                    content=Text(value="Video Quality"),
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                ),
+                Container(
+                    expand = True,
+                    content=single_video_quality,
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                )],spacing=1, alignment="start"),
+                    bgcolor=colors.AMBER_100,
+                ),
+            ]),
+
+        Column([
+                Container(
+                    content=Row([Container(
+                    content=Text(value="Video Format"),
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                ),
+                Container(
+                    expand = True,
+                    content=single_video_format,
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                )],spacing=1, alignment="start"),
+                    bgcolor=colors.AMBER_100,
+                ),
+            ]),
+
+        Column([
+                Container(
+                    content=Row([Container(
+                    content=Text(value="Video Link"),
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                ),
+                Container(
+                    expand = True,
+                    content=single_video_link,
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                )],spacing=1, alignment="start"),
+                    bgcolor=colors.AMBER_100,
+                ),
+            ]),
+
+        Column(
+            expand = True,
+            controls=[
+                Container(
+                    content=Row([Container(
+                    content=Text(value="Output Folder"),
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                ),
+                Container(
+                    expand = True,
+                    content=single_video_folder,
+                    alignment=alignment.center,
+                    width=200,
+                    height=40,
+                    bgcolor=colors.WHITE,
+                )],spacing=1, alignment="start"),
+                    bgcolor=colors.AMBER_100,
+                ),
+            ]),
+        
+        
+    ],spacing=1,width=400,height=500),
+    
+    
+                                    ),
+                            ],
+                            spacing=15
+                        )
+
     singleModeView = Column(
         spacing=5,
         expand=True,
         controls=[
-            
+
                 #1
                 Row([Text(value="Single Mode - Download one Youtube Video", style="headlineSmall")],alignment="center"),
                 
